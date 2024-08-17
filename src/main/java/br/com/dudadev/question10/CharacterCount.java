e390cbaa043d0b60042f4d89aeb49338376111a2
MariaP-Ramalho <eduardapramalho@gmail.com>
2024-08-17 19:55:55 -0300

    feat: answer question 10

---
+++ src/main/java/br/com/dudadev/question10/CharacterCount.java
@@ -0,0 +1,53 @@
+package br.com.dudadev.question10;
+
+
+import java.text.Normalizer;
+import java.util.Scanner;
+
+public class CharacterCount {
+
+    static Scanner sc = new Scanner(System.in);
+
+    private static String removeAccents(String text) {
+        String normalized = Normalizer.normalize(text, Normalizer.Form.NFD);
+        return normalized.replaceAll("\\p{M}", "");
+    }
+
+    public static void characterCount(String text) {
+        if (text == null || text.isEmpty()) {
+            System.out.println("Texto vazio ou nulo.");
+            return;
+        }
+
+        int vowels = 0;
+        int space = 0;
+        int consonants = 0;
+
+        text = text.toLowerCase();
+        text = removeAccents(text);
+
+        for (char c : text.toCharArray()) {
+            if (Character.isWhitespace(c)) {
+                space++;
+            } else if (isVowel(c)) {
+                vowels++;
+            } else if (Character.isLetter(c)) {
+                consonants++;
+            }
+        }
+
+        System.out.println("Quantidade de vogais: " + vowels);
+        System.out.println("Quantidade de consoantes: " + consonants);
+        System.out.println("Quantidade de espaços em branco: " + space);
+    }
+
+    private static boolean isVowel(char c) {
+        return c == 'a' || c == 'e' || c == 'i' || c == 'o' || c == 'u';
+    }
+
+    public static void main(String[] args) {
+        System.out.println("Insira uma frase: ");
+        String text = sc.nextLine();
+        characterCount(text);
+    }
+}
