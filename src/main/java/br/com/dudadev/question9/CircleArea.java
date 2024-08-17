f3bc9df272ebcb33f438a4aeacb8890197df948f
MariaP-Ramalho <eduardapramalho@gmail.com>
2024-08-17 18:25:30 -0300

    feat: answer to question 9

---
+++ src/main/java/br/com/dudadev/question9/CircleArea.java
@@ -0,0 +1,38 @@
+package br.com.dudadev.question9;
+
+import java.util.InputMismatchException;
+import java.util.Scanner;
+
+public class CircleArea {
+    private static final double PI = 3.141592653589793;
+
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        double radius;
+
+        while (true) {
+            try {
+                System.out.print("Informe o raio do círculo: ");
+                radius = scanner.nextDouble();
+
+                if (radius <= 0) {
+                    System.out.println("O raio deve ser um número positivo. Tente novamente.");
+                    continue;
+                }
+
+                double area = PI * Math.pow(radius, 2);
+
+                double roundedArea = Math.round(area);
+
+                System.out.println("A área do círculo é: " + roundedArea);
+                break;
+            } catch (InputMismatchException e) {
+                System.out.println("Entrada inválida. Por favor, insira um número.");
+                scanner.next();
+            }
+        }
+
+        scanner.close();
+    }
+}
+
