759fa546188dd5553cc3c56d27f2b6dc0a5b39d8
MariaP-Ramalho <eduardapramalho@gmail.com>
2024-08-17 16:48:47 -0300

    feat: answer to question 8

--- src/main/java/br/com/dudadev/questao7/DivisaoMultiplosDeTres.java
+++ src/main/java/br/com/dudadev/questao7/DivisaoMultiplosDeTres.java
@@ -6,6 +6,7 @@ public class DivisaoMultiplosDeTres {
         for (int i = 3; i <= 100; i += 3) {
             int resultadoInt = i / 2;
             double resultadoDouble = (double) i / 2;
+
             System.out.println("Divisão de " + i + " por 2 (int): " + resultadoInt + "| Divisão de " + i + " por 2 (double): " + resultadoDouble);
         }
     }
---
+++ src/main/java/br/com/dudadev/questao8/CheckWord.java
@@ -0,0 +1,31 @@
+package br.com.dudadev.questao8;
+
+import java.util.Scanner;
+
+public class CheckWord {
+
+    public static String checkPalindrome(String word) {
+
+        String cleanWord = word.replaceAll("\\s+", "").toLowerCase();
+        int length = cleanWord.length();
+
+        for (int i = 0; i < cleanWord.length() / 2; i++) {
+            if (cleanWord.charAt(i) != cleanWord.charAt(length - i - 1)) {
+                return "A palavra '" + word + "' não é um palíndromo.";
+            }
+        }
+
+        return "A palavra '" + word + "' é um palíndromo.";
+    }
+
+    public static void main(String[] args) {
+
+        Scanner sc = new Scanner(System.in);
+
+        System.out.println("Insira uma palavra para descobrir se é um palíndromo");
+        String word = sc.nextLine();
+
+        System.out.println(checkPalindrome(word));
+    }
+}
+
