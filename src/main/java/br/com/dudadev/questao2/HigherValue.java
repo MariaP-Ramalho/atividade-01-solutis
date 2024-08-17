52dbe318bd454c261106bc112ca2bc1f9fb4c403
MariaP-Ramalho <eduardapramalho@gmail.com>
2024-08-17 16:52:27 -0300

    refactor: rename class and variables

--- src/main/java/br/com/dudadev/questao2/HigherValue.java
+++ src/main/java/br/com/dudadev/questao2/HigherValue.java
@@ -3,20 +3,20 @@ package br.com.dudadev.questao2;
 import java.util.InputMismatchException;
 import java.util.Scanner;
 
-public class MaiorValor {
+public class HigherValue {
 
     public static void main(String[] args) {
 
         try (Scanner sc = new Scanner(System.in)) {
             System.out.print("Digite o primeiro valor: ");
-            int valor1 = sc.nextInt();
+            int value1 = sc.nextInt();
 
             System.out.print("Digite o segundo valor: ");
-            int valor2 = sc.nextInt();
+            int value2 = sc.nextInt();
 
-            int maiorValor = Math.max(valor1, valor2);
+            int higherValue = Math.max(value1, value2);
 
-            System.out.println("O maior valor é: " + maiorValor);
+            System.out.println("O maior valor é: " + higherValue);
         } catch (InputMismatchException e) {
             System.out.println("Entrada inválida. Por favor, digite um número inteiro.");
         }
