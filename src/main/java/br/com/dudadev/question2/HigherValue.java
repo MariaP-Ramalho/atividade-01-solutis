b49e9a2029fe17252fc89a884933c9abc8716de1
MariaP-Ramalho <eduardapramalho@gmail.com>
2024-08-17 18:36:45 -0300

    feat: while loop

--- src/main/java/br/com/dudadev/question2/HigherValue.java
+++ src/main/java/br/com/dudadev/question2/HigherValue.java
@@ -6,19 +6,23 @@ import java.util.Scanner;
 public class HigherValue {
 
     public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        while(true) {
+            try{
+                System.out.print("Digite o primeiro valor: ");
+                int value1 = sc.nextInt();
 
-        try (Scanner sc = new Scanner(System.in)) {
-            System.out.print("Digite o primeiro valor: ");
-            int value1 = sc.nextInt();
+                System.out.print("Digite o segundo valor: ");
+                int value2 = sc.nextInt();
 
-            System.out.print("Digite o segundo valor: ");
-            int value2 = sc.nextInt();
+                int higherValue = Math.max(value1, value2);
 
-            int higherValue = Math.max(value1, value2);
-
-            System.out.println("O maior valor é: " + higherValue);
-        } catch (InputMismatchException e) {
-            System.out.println("Entrada inválida. Por favor, digite um número inteiro.");
+                System.out.println("O maior valor é: " + higherValue);
+                break;
+            } catch (InputMismatchException e) {
+                System.out.println("Entrada inválida. Por favor, digite um número inteiro.");
+                sc.next();
+            }
         }
     }
 }
